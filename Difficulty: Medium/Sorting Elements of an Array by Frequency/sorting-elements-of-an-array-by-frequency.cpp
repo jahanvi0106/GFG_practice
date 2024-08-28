280c9c8f90cbed8c203ee2486a40338627f8f7a6
Jahanvi Gajera <57354105+jahanvi0106@users.noreply.github.com>
2024-08-28 13:18:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+    unordered_map<int, int> m;
+    
+    static bool sortByVal(const pair<int, int>& a, const pair<int, int>& b){
+        if(a.second == b.second) return a.first < b.first;
+            return a.second > b.second;
+    }
+    
+    vector<int> sortByFreq(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int, int>> v;
+        for(int i = 0;i<n;i++){
+            m[arr[i]]++;
+        }
+
+        copy(m.begin(), m.end(), back_inserter(v));
+        sort(v.begin(), v.end(), sortByVal);
+        vector<int> ans;
+        
+        for(int i = 0;i<v.size();i++){
+            for(int j = 0;j<v[i].second;j++){
+                ans.push_back( v[i].first );
+            }
+        }
+        m.clear();
+        return ans;  
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        vector<int> v;
+        v = obj.sortByFreq(arr);
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
