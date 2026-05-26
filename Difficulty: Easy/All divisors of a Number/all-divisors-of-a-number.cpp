625f75c631068517618de017b38d171a30f682d9
Jahanvi Gajera <57354105+jahanvi0106@users.noreply.github.com>
2026-05-26 17:06:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> getDivisors(int n) {
+        // code here
+        vector<int> ans;
+        if(n==1){
+            ans.push_back(1);
+            return ans;
+        }
+        ans.push_back(1);
+        ans.push_back(n);
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0){
+                ans.push_back(i);
+                if(i!=n/i)
+                    ans.push_back(n/i);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
